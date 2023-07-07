24d2e04dd3253adfc4dd99b460a1f81701f40c7e
Alexey Drobinskiy <mralexey@yandex.ru>
2023-07-07 12:33:11 +0300

    Code input processing is suspend fun

--- ui/src/jvmMain/kotlin/com/github/oitc/parser/ui/ViewModel.kt
+++ ui/src/jvmMain/kotlin/com/github/oitc/parser/ui/ViewModel.kt
@@ -5,9 +5,10 @@ import com.github.oitc.parser.generated.TokenMgrError
 import com.github.oitc.parser.ui.domain.ProcessCodeInput
 import com.github.oitc.parser.ui.model.MainScreenState
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.lang.Exception
 
 class ViewModel(private val viewModelCoroutineScope: CoroutineScope) {
 
@@ -26,29 +27,49 @@ class ViewModel(private val viewModelCoroutineScope: CoroutineScope) {
     val screenState: StateFlow<MainScreenState> = _screenState.asStateFlow()
 
     private var codeInputFlow = MutableStateFlow("")
+    private var codeExecutionOutputFlow = MutableStateFlow("")
 
     init {
         viewModelCoroutineScope.launch {
             codeInputFlow
                 .debounce(CODE_INPUT_DEBOUNCE_MS)
-                .collect { codeInput ->
+                .collectAsCodeInput()
+        }
 
-                    var output = try {
-                        ProcessCodeInput.invoke(codeInput)
-                    } catch (ex: ParseException) {
-                        ex.toString()
-                    } catch (ex: TokenMgrError) {
-                        ex.toString()
-                    } catch (ex: Exception) {
-                        "Unknown exception: $ex"
-                    }
-
-                    _screenState.update { currentScreenState ->
-                        currentScreenState.copy(
-                            codeExecutionOutput = output
-                        )
-                    }
-                }
+        viewModelCoroutineScope.launch {
+            codeExecutionOutputFlow
+                .collectAsCodeExecutionOutput()
         }
     }
+
+    private var codeExecutionJob: Job? = null
+
+    private suspend fun Flow<String>.collectAsCodeInput() = collect { codeInput ->
+        codeExecutionJob?.cancel()
+        codeExecutionJob = viewModelCoroutineScope.launch {
+            var output = try {
+                if (isActive) {
+                    ProcessCodeInput.invoke(codeInput)
+                } else {
+                    ""
+                }
+            } catch (ex: ParseException) {
+                ex.toString()
+            } catch (ex: TokenMgrError) {
+                ex.toString()
+            } catch (ex: Exception) {
+                "Unknown exception: $ex"
+            }
+            codeExecutionOutputFlow.emit(output)
+        }
+    }
+
+    private suspend fun Flow<String>.collectAsCodeExecutionOutput() = collect { codeExecutionOutput ->
+        _screenState.update { currentScreenState ->
+            currentScreenState.copy(
+                codeExecutionOutput = codeExecutionOutput
+            )
+        }
+    }
+
 }
--- ui/src/jvmMain/kotlin/com/github/oitc/parser/ui/domain/ProcessCodeInput.kt
+++ ui/src/jvmMain/kotlin/com/github/oitc/parser/ui/domain/ProcessCodeInput.kt
@@ -1,18 +1,19 @@
 package com.github.oitc.parser.ui.domain
 
 import com.github.oitc.parser.generated.Parser
-import java.io.*
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+import java.io.StringReader
 
 
-object ProcessCodeInput :  (String) -> String {
+object ProcessCodeInput : suspend (String) -> String {
 
-    override  fun invoke(codeInput: String): String {
+    override suspend fun invoke(codeInput: String): String {
         val parser = Parser(StringReader(codeInput))
 
         val byteOutStream = ByteArrayOutputStream()
         val outStream = PrintStream(byteOutStream)
         parser.Start(outStream)
-
         return byteOutStream.toString()
     }
 }
